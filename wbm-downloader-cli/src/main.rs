db2ca7388abbb91dbb26c4c5b1f08dc5c86cc8d6
Travis Brown <travisrobertbrown@gmail.com>
2025-11-13 07:00:15 +0100

    Add invalid digest export command

--- wbm-downloader-cli/src/main.rs
+++ wbm-downloader-cli/src/main.rs
@@ -19,6 +19,21 @@ async fn main() -> Result<(), Error> {
             println!("Valid: {}", validation_result.valid_count);
             println!("Invalid: {}", validation_result.errors.len());
         }
+        Command::ExportInvalidDigests { db } => {
+            let db = archivindex_wbm_invalid_log::Database::open(db)?;
+
+            let mut writer = csv::WriterBuilder::new()
+                .has_headers(false)
+                .from_writer(std::io::stdout());
+
+            for result in db.invalid_digests(None)? {
+                let (_, entry) = result?;
+
+                writer.serialize(entry)?;
+            }
+
+            writer.flush()?;
+        }
     }
 
     Ok(())
@@ -38,6 +53,8 @@ pub enum Error {
     StoreIteration(#[from] archivindex_wbm_cas::file::IterationError),
     #[error("Store structure inferrence error")]
     StoreStructureInference(#[from] archivindex_wbm_cas::file::StructureInferenceError),
+    #[error("SQLite error")]
+    Sqlite(#[from] rusqlite::Error),
 }
 
 #[derive(Debug, Parser)]
@@ -54,5 +71,9 @@ enum Command {
     Validate {
         #[clap(long)]
         base: PathBuf,
+    },
+    ExportInvalidDigests {
+        #[clap(long)]
+        db: PathBuf,
     },
 }
