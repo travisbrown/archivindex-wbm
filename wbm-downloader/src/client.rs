0f3e1a12bcaacdad47c15e49d8f1a3e1e6f20ea7
Travis Brown <travisrobertbrown@gmail.com>
2025-11-13 06:59:25 +0100

    More retries

--- wbm-downloader/src/client.rs
+++ wbm-downloader/src/client.rs
@@ -67,7 +67,9 @@ impl Error {
         match self {
             Self::UnexpectedStatus(StatusCode::TOO_MANY_REQUESTS) => true,
             Self::UnexpectedStatus(status_code) if status_code.is_server_error() => true,
-            Self::Client(error) if error.is_timeout() || error.is_body() => true,
+            Self::Client(error) if error.is_timeout() || error.is_body() | error.is_connect() => {
+                true
+            }
             _ => false,
         }
     }
